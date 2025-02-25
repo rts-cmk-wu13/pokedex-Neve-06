c7d02362e3c7afbdff9deeb3e514d83791762f9b
Neve-06 <6255234@rts-365.dk>
2025-02-25 13:59:36 +0100

    added table with contents. these add different pokemon stats

--- details.js
+++ details.js
@@ -15,6 +15,24 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
         <figure class="pokedex__pokemon-details">
         <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
         </figure>
+        <section>
+
+        </section>
+
+        <table class="pokedex__pokemon-stats">
+            <h3>Base Stats</h3>
+            ${pokemon.stats.map(function(singleStat){
+                return `
+                <tr>
+                <th>${singleStat.stat.name}</th>
+                <td>${singleStat.base_stat}</td>
+                <td class="pokedex__pokemon-progressbar"><meter id="file" max="250" value="${singleStat.base_stat}"></td>
+                </tr>
+                `
+            }).join("")
+            }
+        </table>
+
     `
 
 document.querySelector("main").append(sectionElm);
