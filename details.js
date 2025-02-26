0ce720d6b428a295b057855234c13c88774aa066
Neve-06 <6255234@rts-365.dk>
2025-02-26 13:43:47 +0100

    added flavortext

--- details.js
+++ details.js
@@ -6,22 +6,33 @@ console.log(pokeName);
 
 
 let sectionElm = document.createElement("section")
-sectionElm.classList.add("columns")
+// sectionElm.classList.add("columns")
 
 fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
 .then(response => response.json())
 .then(pokemon => {
     console.log(pokemon)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`)
+    .then(response => response.json())
+    .then(species => {
+
+        let flavorTextEntry = species.flavor_text_entries.find(entry => entry.language.name === "en");
+        let flavorText = flavorTextEntry ? flavorTextEntry.flavor_text.replace(/\n|\f/g, ' ') : "No description available.";
+
+
+    
     sectionElm.innerHTML = `
         <figure class="pokedex__pokemon-details">
         <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
         </figure>
+       
+        <div class="pokedex__pokemon-info columns">
         <section>
-            ${pokemon.types.map(function(singleType){
-                return `
-                    <p>${singleType.type.name}</p>
-                `
-            })}
+            <p>${pokemon.weight} kg</p>
+        </section>
+        <section>
+            <p>${pokemon.height} cm</p>
         </section>
         <section>
            ${pokemon.abilities.map(function(singleAbility){
@@ -30,9 +41,13 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
             `
            })}
         </section>
+        <section class="pokedex__pokemon-flavortext">
+        <h3 >${flavorText}</h3>
+        </section>
+
+        </div>
 
         <table class="pokedex__pokemon-stats">
-            <h3>Base Stats</h3>
             ${pokemon.stats.map(function(singleStat){
                 return `
                 <tr>
@@ -46,6 +61,6 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
         </table>
 
     `
-
+})
 document.querySelector("main").append(sectionElm);
 })
